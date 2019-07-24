3ce9810dd64aa4a8e5438da2000d1984cefadb43
William Wong <compulim@hotmail.com>
2019-07-24 02:54:01 -0700

    Update

--- src/webchat/util/createElement.js
+++ src/webchat/util/createElement.js
@@ -1,18 +1,35 @@
-export default function createElement(tagName, attributes, ...children) {
-  const element = document.createElement(tagName);
+const NON_PIXEL_STYLES = [
+  'flex',
+  'z-index',
+  'zoom'
+];
+
+export default function createElement(tag, attributes = {}, ...children) {
+  const element = document.createElement(tag);
 
   Object.keys(attributes).forEach(name => {
     const value = attributes[name];
 
-    if (/^on[A-Z]/.test(name)) {
+    if (name === 'className') {
+      element.className = attributes.className;
+    } else if (/^on[A-Z]/.test(name)) {
       element.addEventListener(name.substr(2).toLowerCase(), value.bind(element));
     } else if (name === 'style') {
-      const styles = value;
-      const elementStyle = element.style;
+      const { style } = attributes;
+      const styleStrings = [];
 
-      Object.keys(styles).forEach(name => {
-        elementStyle[name] = styles[name];
+      Object.keys(style).forEach(name => {
+        let value = style[name];
+        const normalizedName = name.replace(/[A-Z]/gu, c => `-${ c.toLowerCase() }`);
+
+        if (NON_PIXEL_STYLES.includes(normalizedName)) {
+          value += '';
+        }
+
+        styleStrings.push(`${ normalizedName }: ${ typeof value === 'number' ? `${ value }px` : value }`);
       });
+
+      element.setAttribute(name, styleStrings.join('; '));
     } else if (typeof value === 'boolean') {
       value && element.setAttribute(name, '');
     } else if (typeof value !== 'undefined') {
@@ -20,10 +37,12 @@ export default function createElement(tagName, attributes, ...children) {
     }
   });
 
-  const childrenFragment = document.createDocumentFragment();
+  if (children.length) {
+    const fragment = document.createDocumentFragment();
 
-  children.forEach(child => childrenFragment.appendChild(typeof child === 'string' ? document.createTextNode(child) : child));
-  element.appendChild(childrenFragment);
+    children.forEach(child => fragment.appendChild(typeof child === 'string' ? document.createTextNode(child) : child));
+    element.appendChild(fragment);
+  }
 
   return element;
 }
