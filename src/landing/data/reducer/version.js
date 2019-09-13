a3c22ca78178ec61c1e4ee31b66ae7f8455c240c
William Wong <compulim@hotmail.com>
2019-09-13 04:58:34 -0700

    Default to dev build

--- src/landing/data/reducer/version.js
+++ src/landing/data/reducer/version.js
@@ -1,6 +1,6 @@
 import { SET_VERSION } from '../action/setVersion';
 
-export default function (state = 'localhost', action) {
+export default function (state = 'dev', action) {
   if (action.type === SET_VERSION) {
     state = action.payload.version;
   }
