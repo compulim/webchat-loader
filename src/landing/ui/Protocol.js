706ad6289cb0913d6d00a4c42cb793fa56b818db
William Wong <compulim@hotmail.com>
2020-05-13 06:00:29 -0700

    Cosmetics

--- src/landing/ui/Protocol.js
+++ src/landing/ui/Protocol.js
@@ -1,4 +1,5 @@
 import { css } from 'glamor';
+import classNames from 'classnames';
 import React, { useCallback } from 'react';
 
 import Row from './Row';
@@ -14,11 +15,31 @@ const DOMAIN_PREFIX_INSECURE = 'http://';
 const DOMAIN_SUFFIX = '/v3/directline';
 const DOMAIN_SUFFIX_FOR_APP_SERVICE_EXTENSION = '/.bot/v3/directline';
 
-const DOMAIN_INPUT_STYLE = { fontSize: 'inherit' };
-const DOMAIN_STYLE = { fontSize: '80%' };
+const DOMAIN_CSS = css({
+  '&.domain': {
+    fontSize: '80%',
+
+    '& .domain__input': {
+      backgroundColor: '#EEE',
+      border: 0,
+      fontFamily: 'inherit',
+      fontSize: 'inherit',
+      height: '100%',
+      left: 0,
+      position: 'absolute',
+      top: 0,
+      width: '100%'
+    },
+
+    '& .domain__input-box': {
+      margin: 1,
+      position: 'relative'
+    }
+  }
+});
 
 const APP_SERVICE_EXTENSION_CSS = css({});
-const CHECKBOX_STYLE = { margin: 0 };
+const CHECKBOX_CSS = css({ margin: 0 });
 const LABEL_CSS = css({
   alignItems: 'center',
   display: 'flex'
@@ -41,9 +62,9 @@ const Protocol = () => {
         <label className={LABEL_CSS}>
           <input
             checked={protocolWebSocket}
+            className={CHECKBOX_CSS}
             name="protocol"
             onChange={setProtocolWebSocket}
-            style={CHECKBOX_STYLE}
             type="radio"
           />
           &nbsp; Direct Line via Web Socket
@@ -53,9 +74,9 @@ const Protocol = () => {
         <label className={LABEL_CSS}>
           <input
             checked={protocolREST}
+            className={CHECKBOX_CSS}
             name="protocol"
             onChange={setProtocolREST}
-            style={CHECKBOX_STYLE}
             type="radio"
           />
           &nbsp; Direct Line via REST short-polling
@@ -66,25 +87,30 @@ const Protocol = () => {
         <label className={LABEL_CSS}>
           <input
             checked={protocolAppServiceExtension}
+            className={CHECKBOX_CSS}
             name="protocol"
             onChange={setProtocolAppServiceExtension}
-            style={CHECKBOX_STYLE}
             type="radio"
           />
           &nbsp; Direct Line App Service Extension
         </label>
-        <div style={DOMAIN_STYLE}>
-          {protocolAppServiceExtension && isLocalhost(directLineDomainHost) ? DOMAIN_PREFIX_INSECURE : DOMAIN_PREFIX}
-          <input
-            disabled={!protocolAppServiceExtension}
-            onChange={handleDomainChange}
-            required={protocolAppServiceExtension}
-            style={DOMAIN_INPUT_STYLE}
-            type="text"
-            value={protocolAppServiceExtension ? directLineDomainHost : ''}
-          />
-          {protocolAppServiceExtension ? DOMAIN_SUFFIX_FOR_APP_SERVICE_EXTENSION : DOMAIN_SUFFIX}
-        </div>
+        {protocolAppServiceExtension && (
+          <div className={classNames(DOMAIN_CSS + '', 'domain')}>
+            {protocolAppServiceExtension && isLocalhost(directLineDomainHost) ? DOMAIN_PREFIX_INSECURE : DOMAIN_PREFIX}
+            <span className="domain__input-box">
+              <nobr className="domain__doppelganger">{protocolAppServiceExtension ? directLineDomainHost : ''}</nobr>
+              <input
+                className="domain__input"
+                disabled={!protocolAppServiceExtension}
+                onChange={handleDomainChange}
+                required={protocolAppServiceExtension}
+                type="text"
+                value={protocolAppServiceExtension ? directLineDomainHost : ''}
+              />
+            </span>
+            {protocolAppServiceExtension ? DOMAIN_SUFFIX_FOR_APP_SERVICE_EXTENSION : DOMAIN_SUFFIX}
+          </div>
+        )}
         {protocolAppServiceExtension && <small>This protocol is not supported on all versions of Web Chat.</small>}
       </div>
     </Row>
