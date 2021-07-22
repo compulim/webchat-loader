8a2295d480c9c95958011a3dc4e90891a3a7dc48
William Wong <compulim@hotmail.com>
2021-07-22 16:50:14 -0700

    Bump deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const { join } = require('path');
+const { ProvidePlugin } = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -8,12 +9,21 @@ module.exports = {
   },
   mode: 'development',
   output: {
-    filename: '[name].js',
     path: join(__dirname, 'build')
   },
   plugins: [
     new CopyWebpackPlugin({
       patterns: [{ from: 'public', to: '.' }]
-    })
-  ]
+    }),
+    new ProvidePlugin({ process: ['process'] })
+  ],
+  resolve: {
+    fallback: {
+      buffer: require.resolve('buffer'),
+      crypto: require.resolve('crypto-browserify'),
+      process: require.resolve('process/browser'),
+      stream: require.resolve('stream-browserify'),
+      util: require.resolve('util')
+    }
+  }
 };
