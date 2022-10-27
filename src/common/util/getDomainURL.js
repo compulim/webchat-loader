3bd563e1cb6471e70a629c282ccb77caf0a40e2f
William Wong <compulim@users.noreply.github.com>
2022-10-27 02:27:24 +0000

    Fix default domain host

--- src/common/util/getDomainURL.js
+++ src/common/util/getDomainURL.js
@@ -1,17 +1,14 @@
 import isLocalhost from './isLocalhost';
 
 export default function getDomainURL(domainHost, protocol) {
-  if (!domainHost) {
-    throw new Error('domainHost must be specified.');
-  }
-
-  try {
-    if (isLocalhost(domainHost) || protocol === 'app service extension insecure') {
-      return new URL(`http://${domainHost}/.bot/v3/directline`);
-    } else if (protocol === 'app service extension') {
-      return new URL(`https://${domainHost}/.bot/v3/directline`);
-    }
-  } catch (err) {
+  if (domainHost) {
+    try {
+      if (isLocalhost(domainHost) || protocol === 'app service extension insecure') {
+        return new URL(`http://${domainHost}/.bot/v3/directline`);
+      } else if (protocol === 'app service extension') {
+        return new URL(`https://${domainHost}/.bot/v3/directline`);
+      }
+    } catch (err) {}
   }
 
   return new URL(`https://directline.botframework.com/v3/directline`);
