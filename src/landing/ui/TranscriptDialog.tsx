66fcc5b991c433ec2e8ceafb2e84e20d5451d6df
William Wong <compulim@users.noreply.github.com>
2023-10-16 09:29:25 +0000

    Add CSAT

--- src/landing/ui/TranscriptDialog.tsx
+++ src/landing/ui/TranscriptDialog.tsx
@@ -196,6 +196,39 @@ const SAMPLE_TRANSCRIPT_JSON_2 = JSON.stringify(
           actionOption: 'downvote'
         }
       ]
+    },
+    {
+      from: {
+        role: 'bot'
+      },
+      type: 'message',
+      attachments: [
+        {
+          content: {
+            '@context': 'https://schema.org',
+            '@type': 'ReviewAction',
+            actionStatus: 'PotentialActionStatus',
+            description: 'Great! Please rate your experience.',
+            resultReview: {
+              '@type': 'Review',
+              reviewRating: {
+                '@type': 'Rating',
+                'ratingValue-input': {
+                  '@type': 'PropertyValueSpecification',
+                  valueName: 'rate'
+                }
+              }
+            },
+            target: {
+              '@type': 'EntryPoint',
+              actionPlatform: 'https://directline.botframework.com',
+              urlTemplate: 'ms-directline-postback:?valuetype=application/json&value=%7B%22rate%22%3A%22{rate}%22%7D'
+            }
+          },
+          contentType: 'application/ld+json'
+        }
+      ],
+      timestamp: new Date().toISOString()
     }
   ],
   null,
