6dd4c28399db1a7903efcf1bff05a7f9c5e81eb7
William Wong <compulim@hotmail.com>
2024-05-02 01:22:50 -0700

    Save theme to session storage

--- src/webchat/ui/ViaReact.tsx
+++ src/webchat/ui/ViaReact.tsx
@@ -1,12 +1,26 @@
-import React, { Fragment, memo, useState } from 'react';
+import React, { Fragment, memo, useMemo, useState } from 'react';
 import ThemeSwitcher from './ThemeSwitcher';
+
+const SESSION_STORAGE_THEME_KEY = 'THEME';
 
 export default memo(function (props) {
   const {
     WebChat: { FluentThemeProvider, ReactWebChat }
   } = window as any;
 
-  const [mode, setMode] = useState<'fluent' | 'white label'>('white label');
+  const [mode, setMode] = useState<'fluent' | 'white label'>(() => {
+    try {
+      return sessionStorage.getItem(SESSION_STORAGE_THEME_KEY) || 'white label';
+    } catch {}
+
+    return 'white label';
+  });
+
+  useMemo(() => {
+    try {
+      sessionStorage.setItem(SESSION_STORAGE_THEME_KEY, mode);
+    } catch {}
+  }, [mode]);
 
   if (FluentThemeProvider && mode === 'fluent') {
     return (
