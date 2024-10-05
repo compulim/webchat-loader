91461d129922e824a070e2619505ea63dade9e85
William Wong <compulim@hotmail.com>
2024-10-04 20:15:43 -0700

    Move to CSS

--- src/landing/ui/SpeechCredentials.tsx
+++ src/landing/ui/SpeechCredentials.tsx
@@ -1,6 +1,16 @@
-import { css } from 'emotion';
+import './SpeechCredentials.css';
+
 import ms from 'ms';
-import React, { type FocusEventHandler, type MouseEventHandler, useCallback, useEffect, useMemo, useState } from 'react';
+import React, {
+  type ChangeEventHandler,
+  type FocusEventHandler,
+  type MouseEventHandler,
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState
+} from 'react';
 import { looseObject, number, safeParse } from 'valibot';
 
 import useFetchSpeechAuthorizationToken from '../data/hooks/useFetchSpeechAuthorizationToken';
@@ -13,22 +23,6 @@ import isURL from '../util/isURL';
 import tryDecodeJWT from '../util/tryDecodeJWT';
 import Presets from './Presets';
 import Row from './Row';
-
-import type { ChangeEventHandler, FC } from 'react';
-
-const INPUT_CSS = css({ flex: 1, fontFamily: `Consolas, 'Courier New', monospace`, marginRight: '1em' });
-const INPUT_ROW_CSS = css({
-  display: 'flex',
-
-  '& > input:invalid': {
-    borderColor: 'Red',
-    borderStyle: 'solid',
-    borderWidth: 1
-  }
-});
-const RED_CSS = css({ color: 'Red' });
-const REGION_SELECT_CSS = css({ flex: 1 });
-const ROW_CSS = css({ flex: 1 });
 
 const REGIONS: Readonly<Record<string, string>> = Object.freeze({
   westus: 'West US',
@@ -51,7 +45,7 @@ const REGIONS: Readonly<Record<string, string>> = Object.freeze({
   francecentral: 'France Central'
 });
 
-const SpeechCredentials: FC = () => {
+const SpeechCredentials = memo(() => {
   const [authorizationToken, setAuthorizationToken] = useSpeechAuthorizationToken();
   const [region, setRegion] = useSpeechRegion();
   const [savedSubscriptionKeys, saveSubscriptionKey, removeSavedSubscriptionKey] = useSavedSpeechSubscriptionKeys();
@@ -129,10 +123,10 @@ const SpeechCredentials: FC = () => {
   return (
     <React.Fragment>
       <Row header="Speech region">
-        <div className={ROW_CSS}>
-          <div className={INPUT_ROW_CSS}>
+        <div className="speech-credentials__row">
+          <div className="speech-credentials__input-row">
             <select
-              className={REGION_SELECT_CSS}
+              className="speech-credentials__region-select"
               disabled={subscriptionKeyIsURL}
               onChange={handleRegionChange}
               title={subscriptionKeyIsURL ? 'Clear token URL to select region.' : undefined}
@@ -155,10 +149,10 @@ const SpeechCredentials: FC = () => {
         </div>
       </Row>
       <Row header={subscriptionKeyIsURL ? 'Token URL' : 'Speech key'}>
-        <div className={ROW_CSS}>
-          <div className={INPUT_ROW_CSS}>
+        <div className="speech-credentials__row">
+          <div className="speech-credentials__input-row">
             <input
-              className={INPUT_CSS}
+              className="speech-credentials__input"
               disabled={!!authorizationToken}
               onChange={handleSubscriptionKeyChange}
               onFocus={handleSubscriptionKeyFocus}
@@ -212,10 +206,10 @@ const SpeechCredentials: FC = () => {
         </div>
       </Row>
       <Row header="Speech token">
-        <div className={ROW_CSS}>
-          <div className={INPUT_ROW_CSS}>
+        <div className="speech-credentials__row">
+          <div className="speech-credentials__input-row">
             <input
-              className={INPUT_CSS}
+              className="speech-credentials__input"
               onChange={handleAuthorizationTokenChange}
               onFocus={handleAuthorizationTokenFocus}
               required={subscriptionKeyIsURL}
@@ -233,13 +227,13 @@ const SpeechCredentials: FC = () => {
               <small>This token will expire in {ms(timeToExpire, { long: true })}.</small>
             </div>
           ) : (
-            <div className={RED_CSS}>
+            <div className="speech-credentials--red">
               <small>This token has already expired.</small>
             </div>
           ))}
       </Row>
     </React.Fragment>
   );
-};
+});
 
 export default SpeechCredentials;
